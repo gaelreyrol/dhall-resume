ffc6835b98c036349d4c848311d7cd3fc5b9651b
Gaël Reyrol <me@gaelreyrol.dev>
2023-02-11 11:08:51 +0100

    feat: add nix flake package

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "A Dhall package flake";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      rec {
+        devShells = {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              dhall
+              dhall-nixpkgs
+            ];
+          };
+        };
+        packages = flake-utils.lib.flattenTree {
+          default = pkgs.dhallPackages.buildDhallPackage {
+            name = "Resume";
+            code = ./package.dhall;
+          };
+        };
+      }
+    );
+}
